7746a3d5441060defc1dcde60d3847045401e832
John-Tonny <szlhtao@tom.com>
2019-11-18 17:44:11 -0800

    update1

--- vps/main.go
+++ vps/main.go
@@ -20,8 +20,6 @@ func main() {
 	// 将实现服务端的 API 注册到服务端
 	vps.RegisterVpsHandler(srv.Server(), handler.GetHandler(bk))
 
-	handler.VolumeMount("52.14.4.149", "vpub$999000")
-
 	// Run service
 	log.Println("vps running")
 	if err := srv.Run(); err != nil {
